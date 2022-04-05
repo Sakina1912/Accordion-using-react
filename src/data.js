9e15dcb9988c408dc1e9b093df82b6a9412d97ae
Sakina Kagalwala <sakku.kagalwala@gmail.com>
2022-04-05 13:09:15 +0530

    commit

---
+++ src/data.js
@@ -0,0 +1,33 @@
+const questions = [
+    {
+      id: 1,
+      title: 'What is JavaScript used for?',
+      info:
+        'JavaScript is a text-based programming language allows you to make web pages interactive.',
+    },
+    {
+      id: 2,
+      title: 'What is React used for?',
+      info:
+        'React is a free and open-source front-end JavaScript library for building user interfaces or UI components. It is maintained by Facebook and a community of individual developers and companies.',
+    },
+    {
+      id: 3,
+      title: 'What does UI mean?',
+      info:
+        'User interface design or user interface engineering is the design of user interfaces for machines and software, such as computers, home appliances.',
+    },
+    {
+      id: 4,
+      title: 'What is a role of a frontend developer?',
+      info:
+        "A front end developer has one general responsibility: to ensure that website visitors can easily interact with the page. They do this through the combination of design, technology and programming to code a website's appearance, as well as taking care of debugging.",
+    },
+    {
+      id: 5,
+      title: 'When do I recieve a password ordered by letter?',
+      info:
+        'Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan ',
+    },
+  ]
+  export default questions
